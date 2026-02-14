65f6cb90200d8b9ffb99b7990aff146f74cd2f50
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-14 19:16:49 +0500

    first

---
+++ JAVA.java
@@ -0,0 +1,48 @@
+    // Question:
+    // hospital management system in java
+//create a simple hospital management system using oop concepts in Java
+// The system should allow users to odd patients, discharge a patients, display a patient
+// and change patient emergency
+// Use classes, object, inheritance, polymorphism, and encapsulation in your
+// implementation.
+// make a Hospital class to manage the collection of books and user interactions.
+// with methods: add patients, view all paitents, display a paitent, display a paitent,
+// and change patients emergency.
+// A sub class Patient to represent Individual patients with attributes like
+// Name, Age, Disease and Emergency status, and method displayInfo.
+// Also implement user interaction through console input/output
+ //Hints:
+//Use HashMap to store the collection of books in the library class.
+//Key: Patient Nowe, Value: Patient Object
+
+class JAVA {
+
+}
+class hospital{
+    String name;
+    Integer age;
+    String disease;
+    String emergency_status;
+    hospital(String name,Integer age,String disease,String emergency_status){
+        this.name = name;
+        this.age = age;
+        this.disease = disease;
+        this.emergency_status = emergency_status;
+    }
+    void display_info(){
+        System.out.println();
+    }
+    void add_patient(){
+
+    }
+    void view_all_paitents(){
+
+    }
+    void display_a_paitent(){
+
+    }
+    void chg_patients_emg(){
+
+    }
+
+}
