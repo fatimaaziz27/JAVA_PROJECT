a9dcc1b6e8f944aa026f34df9a9b44587149f62a
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-14 21:56:35 +0500

    FIxo

--- JAVA.java
+++ JAVA.java
@@ -17,64 +17,62 @@ import java.util.*;
 //Use HashMap to store the collection of books in the library class.
 //Key: Patient Nowe, Value: Patient Object
 
-class JAVA{
-    public static void main(String[]args){
-        hospital h1 = new hospital("b",2,"f","j");
-        h1.add_patient();
+// class JAVA{
+//     public static void main(String[]args){
+//         hospital h1 = new hospital("b",2,"f","j");
+//         h1.add_patient();
 
-    }
-}
+//     }
+// }
 
 
-class hospital{
-    String name;
-    Integer age;
-    String disease;
-    String emergency_status;
+// class hospital{
+//     String name;
+//     Integer age;
+//     String disease;
+//     String emergency_status;
     
-    hospital(String name,Integer age,String disease,String emergency_status){
-        this.name = name;
-        this.age = age;
-        this.disease = disease;
-        this.emergency_status = emergency_status;
-    }
-}
+//     hospital(String name,Integer age,String disease,String emergency_status){
+//         this.name = name;
+//         this.age = age;
+//         this.disease = disease;
+//         this.emergency_status = emergency_status;
+//     }
+// }
 
-    void add_patient(){
+//     void add_patient(){
 
-        HashMap<Integer,String> patient_names = new HashMap<>();
-        HashMap<Integer,Integer> patient_age = new HashMap<>();
-        HashMap<Integer,String> patient_disease = new HashMap<>();
-        HashMap<Integer,String> patient_emergency_status = new HashMap<>();
+//         HashMap<Integer,String> patient_names = new HashMap<>();
+//         HashMap<Integer,Integer> patient_age = new HashMap<>();
+//         HashMap<Integer,String> patient_disease = new HashMap<>();
+//         HashMap<Integer,String> patient_emergency_status = new HashMap<>();
     
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Do you want to add patient? (yes/no): ");
-        String add = sc.nextLine();
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Do you want to add patient? (yes/no): ");
+//         String add = sc.nextLine();
         
-        System.out.println("Enter patient id: ");
-        int id = sc.nextInt();
+//         System.out.println("Enter patient id: ");
+//         int id = sc.nextInt();
         
-        if (add.equals("yes")){
+//         if (add.equals("yes")){
         
-            System.out.println("Enter patient name: ");
-            String name = sc.nextLine();
-            patient_names.put(id,name);
+//             System.out.println("Enter patient name: ");
+//             String name = sc.nextLine();
+//             patient_names.put(id,name);
 
-            System.out.println("Enter patient age: ");
-            int age = sc.nextInt();
-            patient_age.put(id,age);
+//             System.out.println("Enter patient age: ");
+//             int age = sc.nextInt();
+//             patient_age.put(id,age);
 
-            System.out.println("Enter patient disease: ");
-            String disease = sc.nextLine();
-            patient_disease.put(id,disease);
+//             System.out.println("Enter patient disease: ");
+//             String disease = sc.nextLine();
+//             patient_disease.put(id,disease);
 
-            System.out.println("Emergency? (yes/no): ");
-            String emg = sc.nextLine();
-            patient_names.put(id,emg);
-    }
-}
-
-
+//             System.out.println("Emergency? (yes/no): ");
+//             String emg = sc.nextLine();
+//             patient_names.put(id,emg);
+//     }
+// }
 
     // void display_info(){
         
@@ -220,3 +218,144 @@ class hospital{
 
 
 
+class JAVA{
+    public static void main(String[]args){
+        // hospital h1 = new hospital("b",2,"f","j");
+        // h1.add_patient();
+    }
+}
+
+
+class hospital{
+    String name;
+    Integer age;
+    String disease;
+    String emergency_status;
+    
+    hospital(String name,Integer age,String disease,String emergency_status){
+        this.name = name;
+        this.age = age;
+        this.disease = disease;
+        this.emergency_status = emergency_status;
+    }
+    void dict(){
+        HashMap<Integer,String> patient_names = new HashMap<>();
+        HashMap<Integer,Integer> patient_age = new HashMap<>();
+        HashMap<Integer,String> patient_disease = new HashMap<>();
+        HashMap<Integer,String> patient_emergency_status = new HashMap<>();
+    }
+}
+
+class add_patient extends hospital{
+
+    void add_patient(){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you want to add patient? (yes/no): ");
+        String add = sc.nextLine();
+        
+        System.out.println("Enter patient id: ");
+        int id = sc.nextInt();
+        
+        if (add.equals("yes")){
+        
+            System.out.println("Enter patient name: ");
+            String name = sc.nextLine();
+            patient_names.put(id,name);
+
+            System.out.println("Enter patient age: ");
+            int age = sc.nextInt();
+            patient_age.put(id,age);
+
+            System.out.println("Enter patient disease: ");
+            String disease = sc.nextLine();
+            patient_disease.put(id,disease);
+
+            System.out.println("Emergency? (yes/no): ");
+            String emg = sc.nextLine();
+            patient_names.put(id,emg);
+        }
+
+    }
+}
+
+class display_info extends hospital{
+    
+    void display_info(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter patient ID: ");
+        int id = sc.nextInt();
+        
+        if (patient_name.containskey(id) == true){
+
+            System.out.println(
+            "Patient Info: \nName: " + patient_name.get(id)
+            +"\nAge: " + patient_age.get(id) 
+            + "\nDisease: " + patient_disease.get(id) 
+            + "\nEmergency Status: " + patient_emergency_status.get(id));
+        }
+
+        else{
+            System.out.print("Invalid ID");
+        }
+
+    }
+}
+
+
+
+class view_all_paitents extends hospital{
+
+    void view_all_paitents(){
+        for (int i=0; patient_name.size<i; i++){
+            System.out.println("Patient:" + "\nName: " + patient_name.get(i));
+        }
+    }
+}
+
+
+
+class display_a_paitent extends hospital{ 
+
+    void display_a_paitent(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter patient name: ");
+        String name = sc.nextLine();
+
+        if (patient_name.containskey(name) == true){
+            System.out.println(
+            "Patient Info:"
+            +"\nName: " + patient_name.get(i)
+            +"\nAge: " + patient_age.get(i) 
+            + "\nDisease: " + patient_disease.get(i) 
+            + "\nEmergency Status: " + patient_emergency_status.get(i));
+            }
+        }
+    }
+
+class chg_patients_emg extends hospital{
+    
+    void chg_patients_emg(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter patient ID: ");
+        Integer id = sc.nextInt();
+        
+        if (patient_emergency_status.containskey(id) == true){
+            
+            if (id.equals("yes")){
+                patient_emergency_status.put(id,"No");
+            }
+            
+            else if (id.equals("no")){
+                patient_emergency_status.put(id,"yes");
+            }
+        }
+        
+        else{
+            System.out.print("Invalid ID");
+        }
+
+    }
+}
